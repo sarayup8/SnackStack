5762c5422b72a219c1eb7a582eb34175b8f9a80f
Sarayu Pesala <sarayup@unc.edu>
2026-04-21 14:07:04 -0400

    Initial Commit

---
+++ Snack Stack/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Snack Stack
+//
+//  Created by Sarayu Pesala on 4/21/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Snack Stack/Snack_StackApp.swift
@@ -0,0 +1,17 @@
+//
+//  Snack_StackApp.swift
+//  Snack Stack
+//
+//  Created by Sarayu Pesala on 4/21/26.
+//
+
+import SwiftUI
+
+@main
+struct Snack_StackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
